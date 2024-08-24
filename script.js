83135555c3c53f84063fcbce2fb444d9d3a999d1
Nehemiah Brown <nbrowndeveloper@gmail.com>
2024-08-24 13:16:17 -0500

    photo gallery changes

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
 const recipieSection = document.getElementById("recipie-section");
 
-Promise.all([
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a").then((res) =>
-    res.json()
-  ),
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=b").then((res) =>
-    res.json()
-  ),
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=c").then((res) =>
-    res.json()
-  ),
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=d").then((res) =>
-    res.json()
-  ),
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=e").then((res) =>
-    res.json()
-  ),
-])
-  .then(([aData, bData, cData, dData, eData]) => {
-    const allrecipies = [
-      ...aData.meals,
-      ...bData.meals,
-      ...cData.meals,
-      ...dData.meals,
-      ...eData.meals,
-    ];
-    const selectedRecipieId = ["53049", "52855", "52840", "52899"];
-    const pickedRecipies = allrecipies.filter((recipie) =>
-      selectedRecipieId.includes(recipie.idMeal)
+const fetchPromises = Array.from('abcdefghijklmnopqrstuvwxyz').map(letter =>
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
+    .then(res => res.json())
+);
+
+Promise.all(fetchPromises)
+  .then(responses => {
+    const allRecipies = responses.flatMap(response => response.meals || []);
+
+    const selectedRecipieIds = ["52999", "52855", "52840", '52835', '52931', '52803', '52945', '52908', '52813'];
+
+    const pickedRecipies = allRecipies.filter(recipie =>
+      selectedRecipieIds.includes(recipie.idMeal)
     );
-    console.log(allrecipies);
+
+    console.log(allRecipies);
     displayRecipies(pickedRecipies);
   })
-  .catch((err) => {
+  .catch(err => {
     console.error(`There was an error: ${err}`);
   });
 
 const displayRecipies = (recipies) => {
+  
   recipieSection.innerHTML = "";
 
   recipies.forEach(({ strMealThumb, strMeal, strSource }, index) => {
@@ -44,11 +31,11 @@ const displayRecipies = (recipies) => {
       <div id="${index}" class="recipies">
         <a class="recipie-link" href="${strSource}" target="_blank">
           <div class="recipie-image-container">
-            <img  class="recipie-image" src="${strMealThumb}" alt="${strMeal}">
+            <img class="recipie-image" src="${strMealThumb}" alt="${strMeal}">
             <h1 class="recipie-text">${strMeal}</h1>
           </div>
         </a>
       </div>
-      `;
+    `;
   });
 };
